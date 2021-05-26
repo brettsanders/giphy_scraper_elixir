a1e42868fc3f6fb708309024870d0e2f0cdd2293
Brett Sanders <brettrsanders@gmail.com>
2021-05-26 13:48:06 -0700

    Notes on improvements to make

--- lib/giphy_scraper.ex
+++ lib/giphy_scraper.ex
@@ -1,7 +1,10 @@
 defmodule GiphyScraper do
   @spec search(any) :: list
   def search(query_string \\ "programmer") do
-    http_get_request_from_giphy(query_string)
+    # Use With (possibility of failure)
+    # Stub options -- default arg, change function; or use Adapter; use mocking library
+    query_string
+    |> http_get_request_from_giphy
     |> decode_json_and_return_data_as_map
     |> convert_list_of_maps_to_giphy_image_structs
   end
@@ -16,16 +19,29 @@ defmodule GiphyScraper do
       ]
     ]
 
-    {:ok, %HTTPoison.Response{body: b}} =
-      HTTPoison.request(
-        :get,
-        "https://api.giphy.com/v1/gifs/search",
-        [],
-        [],
-        options
-      )
+    # Jason
+    # Same as below
+    # HTTPoison.get!(
+    #        "https://api.giphy.com/v1/gifs/search",
+    #        []
+    #        options)
 
-    b
+    case HTTPoison.request(
+           :get,
+           "https://api.giphy.com/v1/gifs/search",
+           [],
+           [],
+           options
+         ) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("Not found :(")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
   end
 
   defp decode_json_and_return_data_as_map(json) do
